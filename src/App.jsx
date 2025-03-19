7aa8add8b653d50dff2b76a8afb884a36fda5b47
Abdullah-Nawaf-Saeed-Walidi <abdulla.walidi11@gmail.com>
2025-03-19 23:39:12 +0300

    design almost done

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,49 @@ import Podcast from "./components/Podcast";
 import NewPodcast from "./components/NewPodcast";
 import TeamSection from "./components/TeamSection";
 import Footer from "./components/Footer";
-import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TempContainer from "./components/Generator/TempContainer";
-// import GeneratorPage from "./components/Generator/GeneratorPage";
-
-
 
 function App() {
   return (
     <>
-      
       <BrowserRouter>
         <Routes>
-            <Route index path={"/"} element={
+          <Route
+            index
+            path={"/"}
+            element={
               <>
                 <Header />
-                <div className="text-3xl font-bold text-right py-10 px-20 text-purple-300 rounded-xl   ">
-                  قائمة البودكاست
-                </div>
-                <div  className="container mx-auto  flex flex-col items-center">
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-20">
-                    <NewPodcast />
+                <div className="grid grid-cols-1 items-center" dir="rtl">
+                  <div className="text-3xl font-bold text-right py-10 px-20 text-purple-300 rounded-xl">
+                   بودكاستات مميزة
+                  </div>
+                  <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 p-10">
+                    <Podcast />
+                    <Podcast />
+                    <Podcast />
+                    <Podcast />
                     <Podcast />
                     <Podcast />
                   </div>
-                    <TeamSection />
-                    <Footer />
+                  <div className="text-3xl font-bold text-right py-10 px-20 text-purple-300 rounded-xl">
+                    بودكاستات عامة
+                  </div>
+                  <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 p-10">
+                    <Podcast />
+                    <Podcast />
+                    <NewPodcast />
+                  </div>
+                  <TeamSection />
+                  <Footer />
                 </div>
               </>
-            } />
+            }
+          />
 
-
-           <Route path={"/new_podcast"} element={<TempContainer />} />
+          <Route path={"/new_podcast"} element={<TempContainer />} />
         </Routes>
-     
-      
-
       </BrowserRouter>
     </>
   );
--- src/components/Generator/TempContainer.jsx
+++ src/components/Generator/TempContainer.jsx
@@ -8,45 +8,39 @@ function TempContainer() {
   const [selectedOption, setSelectedOption] = useState(null);
   const [inputValue, setInputValue] = useState("");
 
-
   return (
     <>
-      <nav className="m-0">
-        <div
-          className="flex flex-col md:flex-row items-center justify-between"
-          dir="rtl"
-        >
+      <nav dir="rtl" className="">
+        <div className=" flex  items-center p-0 m-0 justify-around">
           <div className="flex items-center justify-center">
-            <div className="flex items-center justify-between gap-1">
-              {/* Add any additional elements here */}
-            </div>
+            <div className="flex items-center justify-between gap-1"></div>
             <Link to={"/"} className="">
               <img className="w-auto h-28" src={logo} alt="Logo" />
             </Link>
           </div>
 
-          <div className="flex flex-col md:flex-row items-center justify-center text-gray-600 capitalize mt-4 md:mt-0">
+          <div className="flex items-center justify-center  text-gray-600 capitalize ">
             <Link
-              to={"/#main"}
-              className="mx-2 text-[var(--text-color)] border-b-2 border-purple-600"
+              to={"/"}
+              className="mx-2 text-[var(--text-color)] border-b-2 border-purple-600 "
             >
               الرئيسية
             </Link>
             <Link
-              to={"/#podcasts"}
-              className="mx-2 text-[var(--text-color)] border-b-2 border-transparent hover:text-gray-100 hover:border-purple-600 sm:mx-6"
+              to={"/"}
+              className="mx-2 text-[var(--text-color)] border-b-2 border-transparent hover:text-gray-100  hover:border-purple-600 sm:mx-6"
             >
               بودكاستات منشئة
             </Link>
             <Link
-              to={"/#Team"}
-              className="mx-2 text-[var(--text-color)] border-b-2 border-transparent hover:text-gray-100 hover:border-purple-600 sm:mx-6"
+              to={"/"}
+              className="mx-2 text-[var(--text-color)] border-b-2 border-transparent hover:text-gray-100  hover:border-purple-600 sm:mx-6"
             >
               فريق العمل
             </Link>
             <Link
-              to={"/#footer"}
-              className="mx-2 text-[var(--text-color)] border-b-2 border-transparent hover:text-gray-100 hover:border-purple-600 sm:mx-6"
+              to={"/"}
+              className="mx-2 text-[var(--text-color)] border-b-2 border-transparent hover:text-gray-100  hover:border-purple-600 sm:mx-6"
             >
               معلومات التواصل
             </Link>
@@ -55,10 +49,10 @@ function TempContainer() {
       </nav>
 
       <div className="text-[#999] flex flex-col items-center">
-        {/* Main Content */}
-        <div className="container mx-auto flex flex-col md:flex-row gap-10 md:gap-60 mt-10 px-4">
+        
+        <div className="container mx-auto h-96 flex flex-col md:flex-row gap-10 md:gap-60 mt-10 px-20">
           {/* Right Panel - Podcast Preview */}
-          <div className="flex-1 bg-[#222] p-4 md:p-6 rounded-lg shadow-lg flex flex-col items-center mt-6 md:mt-0">
+          <div className="flex-1 bg-[#222] p-4 md:p-2 rounded-lg shadow-lg flex flex-col items-center mt-6 md:mt-0">
             <div className="w-full h-full bg-[#111] flex flex-col gap-5 items-center justify-center rounded-sm p-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -94,8 +88,77 @@ function TempContainer() {
           </div>
 
           {/* Left Panel - Controls */}
-          <div className="px-6 py-8 md:px-10 md:py-32 rounded-lg shadow-md w-full md:w-1/3 text-left shadow-[var(--secondary-color)]">
-            <div className="flex gap-2 items-center">
+          <div className=" md:px-10 md:py-32 rounded-lg w-full md:w-1/3 shadow-md hover:shadow-xl text-left transition duration-200 hover:shadow-[var(--secondary-color)] shadow-[var(--secondary-color)]">
+            <div className="flex items-center gap-3 justify-end ">
+              {/* podcast length */}
+              <div className="relative inline-block ">
+                <label className="block  text-sm font-medium text-left"></label>
+                <button
+                  type="button"
+                  className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-[var(--bg-color)] px-3 py-2 text-sm font-semibold text-[var(--text-color)] ring-1 shadow-xs ring-[#444] ring-inset hover:bg-"
+                  id="menu-button"
+                  aria-expanded={isOpen}
+                  aria-haspopup="true"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {selectedOption || "خيارات"}
+                  <svg
+                    className="-mr-1 size-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+
+                {isOpen && (
+                  <div
+                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[var(--bg-color)] ring-1 shadow-lg ring-black/5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="menu-button"
+                  >
+                    <div className="py-1" role="none">
+                      <button
+                        className="block px-4 py-2 text-sm text-[var(--text-color)] w-full text-left hover:bg-[#444]"
+                        role="menuitem"
+                        onClick={() => {
+                          setSelectedOption("قصير");
+                          setIsOpen(false);
+                        }}
+                      >
+                        قصير
+                      </button>
+                      <button
+                        className="block px-4 py-2 text-sm text-[var(--text-color)] w-full text-left hover:bg-[#444]"
+                        role="menuitem"
+                        onClick={() => {
+                          setSelectedOption("متوسط");
+                          setIsOpen(false);
+                        }}
+                      >
+                        متوسط
+                      </button>
+                      <button
+                        className="block px-4 py-2 text-sm text-[var(--text-color)] w-full text-left hover:bg-[#444]"
+                        role="menuitem"
+                        onClick={() => {
+                          setSelectedOption("طويل");
+                          setIsOpen(false);
+                        }}
+                      >
+                        طويل
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+
               <input
                 dir="rtl"
                 type="text"
@@ -103,80 +166,10 @@ function TempContainer() {
                 placeholder="اكتب الموضوع هنا. . . . ."
               />
             </div>
-
-            {/* podcast length */}
-            <div className="relative inline-block ml-60 ">
-              <label className="block mt-4 mb-2 text-sm font-medium text-left"></label>
-              <button
-                type="button"
-                className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-[var(--bg-color)] px-3 py-2 text-sm font-semibold text-[var(--text-color)] ring-1 shadow-xs ring-[#444] ring-inset hover:bg-"
-                id="menu-button"
-                aria-expanded={isOpen}
-                aria-haspopup="true"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                {selectedOption || "خيارات"}
-                <svg
-                  className="-mr-1 size-5 text-gray-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-
-              {isOpen && (
-                <div
-                  className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[var(--bg-color)] ring-1 shadow-lg ring-black/5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="menu-button"
-                >
-                  <div className="py-1" role="none">
-                    <button
-                      className="block px-4 py-2 text-sm text-[var(--text-color)] w-full text-left hover:bg-[#444]"
-                      role="menuitem"
-                      onClick={() => {
-                        setSelectedOption("قصير");
-                        setIsOpen(false);
-                      }}
-                    >
-                      قصير
-                    </button>
-                    <button
-                      className="block px-4 py-2 text-sm text-[var(--text-color)] w-full text-left hover:bg-[#444]"
-                      role="menuitem"
-                      onClick={() => {
-                        setSelectedOption("متوسط");
-                        setIsOpen(false);
-                      }}
-                    >
-                      متوسط
-                    </button>
-                    <button
-                      className="block px-4 py-2 text-sm text-[var(--text-color)] w-full text-left hover:bg-[#444]"
-                      role="menuitem"
-                      onClick={() => {
-                        setSelectedOption("طويل");
-                        setIsOpen(false);
-                      }}
-                    >
-                      طويل
-                    </button>
-                  </div>
-                </div>
-              )}
-            </div>
-
-            <div className="flex flex-col gap-6 mt-6">
-              <button className="bg-[var(--primary-color)] font-bold text-[var(--text-color)] px-12 transition duration-300 py-3 rounded-xl mb-5 hover:bg-[var(--third-color)]">
-                أنشئ بودكاست
-              </button>
+            <div className="flex flex-col gap-3 mt-10">
+                <button className="bg-[var(--primary-color)] font-bold text-[var(--text-color)] px-12 transition duration-300 py-3 rounded-xl mb-5 hover:bg-[var(--third-color)]">
+                  أنشئ بودكاست
+                </button>
               <div className="flex gap-6 justify-center items-center">
                 <button className="bg-[var(--bg-color)] border-2 px-10 py-2 transition duration-200 rounded-2xl text-[var(--text-color)] border-[#444] hover:bg-[#222]">
                   احفظ
