7a49c6a8f55e3c30deaf8d81802a665b104e6d4e
Ali Alahdal <aloshegamer@gmail.com>
2025-03-20 14:32:25 +0300

    wda

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Podcast from "./components/Podcast";
 import NewPodcast from "./components/NewPodcast";
 import TeamSection from "./components/TeamSection";
 import Footer from "./components/Footer";
-import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TempContainer from "./components/Generator/TempContainer";
 import PodcastGenerator from "./components/Generator/GeneratorPage";
 import ImageGenerator from "./components/Generator/ImageGenerator";
@@ -13,46 +13,49 @@ import ImageGenerator from "./components/Generator/ImageGenerator";
 
 
 
-
 function App() {
   return (
     <>
-      
       <BrowserRouter>
         <Routes>
-            <Route index path={"/"} element={
+          <Route
+            index
+            path={"/"}
+            element={
               <>
                 <Header />
-                <div className="text-3xl font-bold text-right py-10 px-20 text-purple-300 rounded-xl   ">
-                  قائمة البودكاست
-                </div>
-                <div  className="container mx-auto  flex flex-col items-center">
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-20">
-                    <NewPodcast />
+                <div className="grid grid-cols-1 items-center" dir="rtl">
+                  <div className="text-3xl font-bold text-right py-10 px-20 text-purple-300 rounded-xl">
+                   بودكاستات مميزة
+                  </div>
+                  <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 p-10">
+                    <Podcast />
+                    <Podcast />
+                    <Podcast />
+                    <Podcast />
                     <Podcast />
                     <Podcast />
                   </div>
-                    <TeamSection />
-                    <Footer />
+                  <div className="text-3xl font-bold text-right py-10 px-20 text-purple-300 rounded-xl">
+                    بودكاستات عامة
+                  </div>
+                  <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 p-10">
+                    <Podcast />
+                    <Podcast />
+                    <NewPodcast />
+                  </div>
+                  <TeamSection />
+                  <Footer />
                 </div>
               </>
             } />
 
 
-
            <Route path={"/new_podcast"} element={<TempContainer />} />
            <Route path={"/new_podcast2"} element={<PodcastGenerator />} />
            <Route path={"/img"} element={<ImageGenerator />} />
-           <Route path={"/new_podcast"} element={<TempContainer />} />
-
-
-
-    
-
+          <Route path={"/new_podcast"} element={<TempContainer />} />
         </Routes>
-     
-      
-
       </BrowserRouter>
     </>
   );
